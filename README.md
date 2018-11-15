bac892497a81ae15929618010c1801b5c3254b52
Stefan Malzner <stefan@adlk.io>
2018-11-15 15:37:35 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# macos-isDarkMode
+Simple module to check if macOS Mojave dark mode is enabled
+
+This module is ready to be used in electron.
+
+## How to use
+Install the module `npm install @adlk/mojave-isdarkmode`.
+
+```js
+import isDarkMode from "@adlk/mojave-isdarkmode";
+
+async function config() {
+  const isDarkModeEnabled = await isDarkModeEnabled();
+
+  return {
+    randomAppConfig: true,
+    minimizeToSystemTray: false,
+    isDarkModeEnabled,
+  };
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+
+import { exec } from "child_process";
+import debug from "debug";
+import os from "os";
+
+const d = debug("mojave-isDarkMode");
+
+export default async function isDarkModeEnabled(): Promise<boolean | null> {
+    if (process.platform === "darwin" && parseInt(os.release().split(".")[0], 10) >= 18) {
+      d("Checking if macOS mojave dark mode is enabled");
+      try {
+        const configPromise = new Promise(((resolve, reject) => {
+          exec('defaults read "Apple Global Domain" "AppleInterfaceStyle"', (error, stdout, stderr) => {
+            if (error || stderr) {
+              reject(error || stderr);
+            }
+
+            resolve(stdout.trim());
+          });
+        }));
+
+        const config = await configPromise;
+        d("macOS dark mode is enabled");
+
+        return config === "Dark";
+      } catch (err) {
+        // `AppleInterfaceStyle` doesn't exist if dark mode isn't enabled
+        d("macOS dark mode is disabled");
+        return false;
+      }
+
+    } else {
+      if (process.platform !== "darwin") {
+        console.log("Platform is not macOS/darwin");
+      } else {
+        if (process.platform !== "darwin") {
+          console.log("Dark mode is only available on macOS mojave or later");
+        }
+      }
+
+      return null;
+    }
+}
